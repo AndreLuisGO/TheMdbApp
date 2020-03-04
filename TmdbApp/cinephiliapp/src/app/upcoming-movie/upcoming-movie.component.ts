b0632535f7904ccfca61b0dab419da227bd817f3
AndreLuisGO <algo1305@gmail.com>
2020-03-04 03:13:54 -0300

    Creates service to call api
    - Service calls to API
    - Partially creates upcoming movies
    - Rearranges app placement on start page.

--- TmdbApp/cinephiliapp/src/app/upcoming-movie/upcoming-movie.component.ts
+++ TmdbApp/cinephiliapp/src/app/upcoming-movie/upcoming-movie.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { TmdbService } from '../tmdb.service';
 
 @Component({
   selector: 'app-upcoming-movie',
   templateUrl: './upcoming-movie.component.html',
-  styleUrls: ['./upcoming-movie.component.css']
+  styleUrls: ['./upcoming-movie.component.css'],
+  providers: [TmdbService]
 })
 export class UpcomingMovieComponent implements OnInit {
 
-  constructor() { }
+  movies;
 
-  ngOnInit(): void {
+  constructor(private tmdbService: TmdbService) { }
+
+  getUpcomingMovies(page: number) {
+    this.tmdbService.getUpcoming(page).subscribe(
+      data => {
+        this.movies = data;
+        console.log(data);
+      },
+      err => console.log(err),
+      () => console.log(`success`)
+    );
+
+  }
+
+  ngOnInit() {
   }
 
 }
