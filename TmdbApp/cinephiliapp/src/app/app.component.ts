59fced27e3b931017d6b8f2f208808a09e0abd36
Andre Luis Goncalves Oliveira <algo1305@gmail.com>
2020-03-11 00:37:39 -0300

    Update app.component.ts
    removes unnecessary code

--- TmdbApp/cinephiliapp/src/app/app.component.ts
+++ TmdbApp/cinephiliapp/src/app/app.component.ts
@@ -9,7 +9,6 @@ export class AppComponent{
   
   title = 'CinephiliApp';
   private searchQuery: string;
-  autocompleteSearch;
   private pageNumber: number = 1;
   constructor(private tmdbService: TmdbService){}
 
@@ -17,24 +16,7 @@ export class AppComponent{
   public searchMoviesNavBar() {
     console.log('searchMoviesNavBar: ' + this.searchQuery)
   }
-
-  // public autoCompleteSearch(){
-  //   if (this.searchQuery.length > 2){
-  //     this.tmdbService.searchMovie(this.searchQuery).subscribe(
-  //       data => {
-  //         this.autoCompleteSearch = data;
-  //         console.log(data);
-  //       },
-  //         err => console.log(err),
-  //         () => console.log(`success`)
-  //       );
-  //   } else {
-  //   }
-  //}
   
-
-
-
   public selectNavBar(movie) {
     this.searchQuery = movie;
   }
